c5eeca1c85459d396d85eba4a4b765f5f147def4
chetanabhatstudio <chetanabhatstudio@gmail.com>
2023-06-10 06:33:26 +0100

    sanity update

---
+++ dist/static/index-c3743809-38fce9f0.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-df9a8d3b.js";import"./sanity-5cecff1e.js";import"./json-inspector-27a389d9.js";export{m as DocumentPaneProvider,a as default};
--- sanity/env.ts
+++ sanity/env.ts
@@ -2,9 +2,9 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-06-09'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
+);
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,

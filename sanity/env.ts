36854311fa05a03b48839f1a7551150c1dc061e9
chetanabhatstudio <chetanabhatstudio@gmail.com>
2023-06-10 06:55:30 +0100

    clean up login

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,7 +2,7 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-06-09'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
   "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
 );
 
